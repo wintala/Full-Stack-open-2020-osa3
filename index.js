fbeb1906e39dcb80e3db03e4c3c496f46295032e
Waltteri Rintala <rintala.waltteri@gmail.com>
2020-06-09 20:48:52 +0300

    changed position of use(cors)

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(cors)
 app.use(express.json())
-app.use(cors())
 app.use(express.static('build'))
 
 morgan.token('data', (req, res) => { return JSON.stringify(req.body)})
